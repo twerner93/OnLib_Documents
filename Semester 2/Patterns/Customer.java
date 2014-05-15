f8feb11ad1a50286f0a6b69610205838bd585795
wanping1994 <wanping.qu@gmx.de>
2014-05-15 13:09:46 +0200

    Extracted Method getFrequentRenterPoints()

--- Semester 2/Patterns/Customer.java
+++ Semester 2/Patterns/Customer.java
@@ -23,12 +23,7 @@ class Customer {
 		while (rentals.hasMoreElements()) {
 			double thisAmount = 0;
 			Rental each = (Rental) rentals.nextElement();
-			// add frequent renter points
-			frequentRenterPoints ++;
-			// add bonus for a two day new release rental
-			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-			&&
-			each.getDaysRented() > 1) frequentRenterPoints ++;
+			frequentRenterPoints += each.getFrequentRenterPoints();
 			//show figures for this rental
 			result += "\t" + each.getMovie().getTitle()+ "\t" +
 			String.valueOf(each.getCharge()) + "\n";
--- Semester 2/Patterns/Rental.java
+++ Semester 2/Patterns/Rental.java
@@ -29,5 +29,12 @@ class Rental {
 				break;
 		}
 		return result;
-}
+	}
+	public int getFrequentRenterPoints() {
+		if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+		getDaysRented() > 1)
+			return 2;
+		else
+			return 1;
+	}
 }
