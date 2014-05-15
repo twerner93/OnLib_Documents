07848be0b98bb836e24b1d7aa351ee8635e9bfd0
wanping1994 <wanping.qu@gmx.de>
2014-05-15 13:17:22 +0200

    Replaced Temp (totalAmount and frequentRenterPoints) with Query

--- Semester 2/Patterns/Customer.java
+++ Semester 2/Patterns/Customer.java
@@ -27,12 +27,29 @@ class Customer {
 			//show figures for this rental
 			result += "\t" + each.getMovie().getTitle()+ "\t" +
 			String.valueOf(each.getCharge()) + "\n";
-			totalAmount += each.getCharge();
 		}
 		//add footer lines
-		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+		result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
 		result += "You earned " +
-			String.valueOf(frequentRenterPoints) + " frequent renter points";
+			String.valueOf(getTotalFrequentRenterPoints) + " frequent renter points";
+		return result;
+	}
+	private double getTotalCharge() {
+		double result = 0;
+		Enumeration rentals = _rentals.elements();
+		while (rentals.hasMoreElements()) {
+			Rental each = (Rental) rentals.nextElement();
+			result += each.getCharge();
+		}
+		return result;
+	}
+	private int getTotalFrequentRenterPoints(){
+		int result = 0;
+		Enumeration rentals = _rentals.elements();
+		while (rentals.hasMoreElements()) {
+			Rental each = (Rental) rentals.nextElement();
+			result += each.getFrequentRenterPoints();
+		}
 		return result;
 	}
 	private double amountFor(Rental aRental) {
