c83b33b4d7aacd0ac9f34524ff84d062a16b6f87
wanping1994 <wanping.qu@gmx.de>
2014-05-15 13:05:28 +0200

    replaced Temp with Query

--- Semester 2/Patterns/Customer.java
+++ Semester 2/Patterns/Customer.java
@@ -23,9 +23,7 @@ class Customer {
 		while (rentals.hasMoreElements()) {
 			double thisAmount = 0;
 			Rental each = (Rental) rentals.nextElement();
-			thisAmount = each.getCharge();
 			// add frequent renter points
-			25
 			frequentRenterPoints ++;
 			// add bonus for a two day new release rental
 			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
@@ -33,8 +31,8 @@ class Customer {
 			each.getDaysRented() > 1) frequentRenterPoints ++;
 			//show figures for this rental
 			result += "\t" + each.getMovie().getTitle()+ "\t" +
-			String.valueOf(thisAmount) + "\n";
-			totalAmount += thisAmount;
+			String.valueOf(each.getCharge()) + "\n";
+			totalAmount += each.getCharge();
 		}
 		//add footer lines
 		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
